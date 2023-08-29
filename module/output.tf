56713b9511fbc465c76e7a44c3c6bc60b311e6f8
Atisha Shaurya <94087803+Atisha-Shaurya@users.noreply.github.com>
2023-08-29 11:12:39 +0530

    Update output.tf

--- module/output.tf
+++ module/output.tf
@@ -1,7 +1,1 @@
-output "private_dns_zone_ids" {
-  value = [for dnszone in azurerm_private_dns_zone.dnszone : dnszone.id]
-}
 
-output "private_dns_zone_virtual_network_link_ids" {
-  value = [for vnl in azurerm_private_dns_zone_virtual_network_link.vnl : vnl.id]
-}
